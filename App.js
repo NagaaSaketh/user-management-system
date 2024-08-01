6830e979d36f67cdd30d9410881e4a75c5d442f8
Rampranav <vrampranav9@gmail.com>
2024-08-01 22:25:20 +0530

    refactor:Initial Setup

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const AppLayout = ()=>{
+  return <h1>Welcome!</h1>
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<AppLayout/>)
